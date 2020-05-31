624fa4acc341d55bafd3ec9e171f55ca60525fd4
suesho <35975764+suesho@users.noreply.github.com>
2020-05-31 18:24:09 +0900

    Spring Boot2 のアプリケーションとして動作するよう設定

---
+++ uni-std/src/main/java/unistd/Controller.java
@@ -0,0 +1,13 @@
+package unistd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class Controller {
+
+	@GetMapping
+	public String ping() {
+		return "OK!";
+	}
+}
