a547b4a3ff8bf7ab1635eba6fec1e3503f87069d
suesho <35975764+suesho@users.noreply.github.com>
2020-06-30 22:55:43 +0900

    Thymeleafが動くところまで作成

---
+++ study-project/src/main/java/unistd/HelloController.java
@@ -0,0 +1,17 @@
+package unistd;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+
+        model.addAttribute("message", "Hello Thymeleaf!");
+        return "hello";
+    }
+}
