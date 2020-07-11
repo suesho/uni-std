8fd0564dd79208e1086015a069bf87461f4533d6
suesho <35975764+suesho@users.noreply.github.com>
2020-07-11 16:59:48 +0900

    PUT/DELETEが動くように設定値を変更、APIのパスを修正

--- study-project/src/main/java/unistd/api/UserController.java
+++ study-project/src/main/java/unistd/api/UserController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import lombok.RequiredArgsConstructor;
 import unistd.domain.service.model.User;
@@ -53,11 +55,10 @@ public class UserController {
         return "editUser";
     }
 
-    @PostMapping("/user/{userId}")
+    @PutMapping("/user/{userId}")
     public String updateUser(@PathVariable("userId") int userId, @ModelAttribute("user") User user) {
 
         user.setUserId(userId);
-
         service.updateUser(user);
 
         return "redirect:/";
@@ -72,7 +73,7 @@ public class UserController {
         return "deleteUser";
     }
 
-    @GetMapping("/user/{userId}/delete")
+    @DeleteMapping("/user/{userId}")
     public String deleteUser(@PathVariable("userId") int userId) {
 
         service.deleteUser(userId);
